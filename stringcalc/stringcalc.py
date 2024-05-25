af9d5e8818b8215288cf96f5fb8ca89db9018f86
Krishna Manoj <krishnamanoj452@gmail.com>
2024-05-25 16:10:55 +0530

    Test Passed - String contains non numeric characters.

--- stringcalc/stringcalc.py
+++ stringcalc/stringcalc.py
@@ -2,12 +2,11 @@
 
 class StringCalculator:
 
-    def add(self,str):
-        sum = 0
-        if str == "":
+    def add(self,input_string):
+        if not input_string:
             return 0
-        else:
-            for i in str:
-                if i != ',':
-                    sum += int(i)
-            return sum
+        numbers = input_string.split(',')
+        try:
+            return sum(int(num) for num in numbers)
+        except ValueError:
+            raise ValueError("Input string contains non numeric data.")
--- tests/test_stringcalc.py
+++ tests/test_stringcalc.py
@@ -8,14 +8,25 @@ class TestStringCalculator(unittest.TestCase):
     def setUp(self):
         self.StringCal = StringCalculator()
 
-    def test_empty_string_returns_zero(self):
+    def test_empty_string_returns_zero(self): #Test empty string
         result = self.StringCal.add("")
         self.assertEqual(0, result)
     
-    def test_string_number_returns_sum(self):
+    def test_string_numbers_returns_sum(self): #Test digit
         result = self.StringCal.add("1")
         self.assertEqual(1,result)
-    
-    def test_string_numbers_returns_sum(self):
+        
+        #Test single digits
         result = self.StringCal.add("1,5")
         self.assertEqual(6,result)
+
+        #Test 2/3/4 digit numbers
+        result = self.StringCal.add("3000,40,289")
+        self.assertEqual(3329,result)
+
+        #Test any amount of numbers
+        result = self.StringCal.add("1,5,6,7,8,9,10,44,50,31,22,1,10,20,30,40,50,60,70,80,90,21,76")
+        self.assertEqual(741,result)
+
+    def test_string_non_numbers_raises_error(self): #If string contains characters
+        self.assertRaises(ValueError, self.StringCal.add, "h,j,k,l")
